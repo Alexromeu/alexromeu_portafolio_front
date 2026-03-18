877c7636f50ba0578164335c28c200f6e1657524
Alexromeu <alexxromeu@gmail.com>
2026-03-17 20:56:04 -0500

    domain added

--- src/utils/parseUrl.jsx
+++ src/utils/parseUrl.jsx
@@ -1,6 +1,6 @@
 export default function parseURL(route) {
-  //const url = 'https://alexromeu-portafolio-backend.onrender.com'
-   const url = 'http://127.0.0.1:3000'
+  const url = 'https://alexromeu-portafolio-backend.onrender.com'
+  //const url = 'http://127.0.0.1:3000'
   return `${url}${route}`
 }
 
