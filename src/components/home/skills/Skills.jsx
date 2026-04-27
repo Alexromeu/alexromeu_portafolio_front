ecb9399abe58a591319e8f40ff86f748c310e387
Alexromeu <alexxromeu@gmail.com>
2026-04-27 11:22:45 -0500

    changes: cloudflare analitics added

--- src/components/home/skills/Skills.jsx
+++ src/components/home/skills/Skills.jsx
@@ -1,11 +1,8 @@
 import "../../../styles/skills.css"
-import { useState } from "react"
 
 export default function Skills() {
-    const [ hoveredSkill, setHoverSkill ] = useState(null)
-
     const skills = [
-  { skill: "react", url:"/icons/skills/React-Dark.svg"},      
+  { skill: "react", url:"/icons/skills/React-Dark.svg"},
   { skill: "JavaScript", url: "/icons/skills/JavaScript.svg" },
   { skill: "nodejs", url: "/icons/skills/NodeJS-Dark.svg" },
   { skill: "SQL", url: "/icons/skills/MySQL-Dark.svg" },
@@ -14,27 +11,24 @@ export default function Skills() {
   { skill: "TypeScript", url:"/icons/skills/TypeScript.svg"},
   { skill: "css", url: "/icons/skills/CSS.svg" },
   { skill: "html", url: "/icons/skills/HTML.svg" },
-  
+
 ]
-   
+
     return (<>
     <section className="skills-section" id="skills">
      <h1>Skills</h1>
-     <div className="skills-container">  
+     <div className="skills-container">
         {skills.map(({ skill, url }) => (
-            <div key={skill} className="skill-icon">
-                <img 
-                src={url} 
-                key={`${skill}-img`} 
-                onMouseEnter={() => setHoverSkill(skill)}
-                onMouseLeave={() => setHoverSkill(false)}
+            <div key={skill} className="skill-icon" tabIndex={0}>
+                <img
+                src={url}
+                key={`${skill}-img`}
                 />
-                
-                { hoveredSkill === skill && <span className="skill-name">{skill}</span>}
+                <span className="skill-name">{skill}</span>
                 </div>
         ))}
      </div>
     </section>
-    
+
     </>)
 }
